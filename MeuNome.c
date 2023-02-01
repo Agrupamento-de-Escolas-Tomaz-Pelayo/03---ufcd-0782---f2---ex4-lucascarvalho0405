4ba40594dca4d65a886eaa186b1e12db20c75dbc
lucascarvalho0405 <a31559@tomazpelayo.com>
2023-02-01 14:19:08 +0000

    f2-ex4

--- MeuNome.c
+++ MeuNome.c
@@ -1,3 +1,8 @@
 // Resolva neste ficheiro o seu exercício
+#include<stdio.h>
+int main()
+{
+    printf("Nome : Carlos Fonseca\n Data : 14 de Setembro de 2007\n Morada : Santo Tirso. ");
+    return(0);
 
-
+}
